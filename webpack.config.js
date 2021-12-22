f78166debac1c5ec0891cdfd7b8a6da2aedba54c
Quan <pdmquan@gmail.com>
2021-12-22 22:49:14 +0700

    Release version 1.0.0

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+// webpack.config.js
+
+const {resolve} = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    entry: resolve(__dirname, 'src/index.js'),
+    output: {
+        path: resolve(__dirname, 'dist'),
+        filename: 'send-email.js',
+        library: 'sendemail'
+    },
+    plugins: [
+        new UglifyJsPlugin({
+            exclude: [/\.min\.js$/gi] // skip pre-minified libs
+        })
+    ]
+}
+
